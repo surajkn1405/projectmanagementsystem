2ea69ea1cb469418e04b59c848fb950de97da30d
Suraj Kumar Nayak <surajkn1405@gmail.com>
2024-08-10 22:56:31 +0530

    initial commit

--- projectmanagementsystem/src/main/java/com/suraj/repository/ProjectRepository.java
+++ projectmanagementsystem/src/main/java/com/suraj/repository/ProjectRepository.java
@@ -3,8 +3,6 @@ package com.suraj.repository;
 import com.suraj.modal.Project;
 import com.suraj.modal.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
